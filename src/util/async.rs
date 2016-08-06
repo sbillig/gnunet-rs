2a87f3baa2790bea46b91f51eea61c21f7e52d0c
kc1212 <kc1212@users.noreply.github.com>
2016-08-06 22:25:28 +1200

    Test for cancelling promises

--- src/util/async.rs
+++ src/util/async.rs
@@ -32,3 +32,15 @@ pub trait PromiseReader {
     fn read_u16(&mut self) -> Promise<u16, Error>;
     fn read_u32(&mut self) -> Promise<u32, Error>;
 }
+
+#[test]
+fn test_gjio_cancel() {
+    EventLoop::top_level(move |wait_scope| -> Result<(), ::std::io::Error> {
+        let mut event_port = EventPort::new().unwrap();
+        match cancel(Promise::<(), Error>::never_done()).wait(wait_scope, &mut event_port) {
+            Ok(_) => assert!(false, "Promise did not cancel"),
+            Err(e) => assert_eq!(e.kind(), ErrorKind::Interrupted),
+        };
+        Ok(())
+    }).expect("top level");
+}
